75e11af3314e27622645fe031c129d8e7cc764c7
Nizam <nizamnizs786@gmail.com>
2023-08-21 11:13:52 +0530

    first commit

---
+++ middlewares/routerMiddleware.js
@@ -0,0 +1,23 @@
+ const jwt=require('jsonwebtoken')
+
+//middleware 
+//  a function with 3 arguments  - req, res, next
+
+ const jwtMiddleware=(req,res,next)=>{
+
+try{
+    //access token from request header
+    const token=req.headers["access_token"] //incase can't access token  - runtime error (unexpected error-situation based)  - try catch
+    
+    //validate token - jwt - verify()
+    jwt.verify(token,"secretkey123")  // true/false
+   
+    //if token is verified continue the request
+    next()
+}
+catch{
+
+    res.status(404).json("please login")
+}
+}
+ module.exports=jwtMiddleware
---
+++ models/modelcollection.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+
+const users=new mongoose.model("users",{
+    acno:Number,
+    uname:String,
+    psw:String,
+    balance:Number,
+    transaction:[]
+})
+module.exports=users
